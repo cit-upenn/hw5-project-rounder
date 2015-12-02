65f6ec6d48b08af8193058c52e88dddc24dd97a5
Zhiyuan Li <zhil@seas.upenn.edu>
2015-12-02 16:27:44 -0500

    all buttons to arrary

--- src/roulette/RouletteView.java
+++ src/roulette/RouletteView.java
@@ -23,8 +23,6 @@ public class RouletteView extends JFrame {
 	private JPanel subTop, subLeft, subRight, subBottom, subCenter;
 	private JPanel subBottom1, subBottom2, subBottom3;
 	private JButton[] buttons;
-	private JButton b1stColumn, b2ndColumn, b3rdColumn, b1stDozen, b2ndDozen, b3rdDozen, b1stHalf, b2ndHalf, bOdd,
-			bEven, bRed, bBlack;
 	private static final Border WHITE_BORDER = new LineBorder(Color.WHITE, 2);
 	private static final Color DARK_GREEN = new Color(0, 100, 0);
 	private static final Color FOREST_GREEN = new Color(34, 139, 34);
@@ -111,11 +109,11 @@ public class RouletteView extends JFrame {
 	private void addButtons() {
 
 		// initializing
-		buttons = new JButton[37];
-		
+		buttons = new JButton[49];
+
 		// set up every button color and font
 		for (int i = 0; i < buttons.length; i++) {
-			buttons[i] = new JButton("" + i);
+			buttons[i] = new JButton(buttonIndexToString(i));
 			buttons[i].setForeground(Color.white);
 			buttons[i].setOpaque(true);
 			buttons[i].setBorderPainted(true);
@@ -172,65 +170,32 @@ public class RouletteView extends JFrame {
 
 				buttons[i].setBackground(Color.black);
 			}
-			
-		}
 
+		}
+		for (int i = 29; i <= 36; i++) {
+
+			if (i % 2 == 0) {
+				buttons[i].setBackground(Color.red);
+			} else {
+
+				buttons[i].setBackground(Color.black);
+			}
+
+		}
 		// add 1st-3rd column buttons
 		subRight.setLayout(new GridLayout(3, 1));
-		b1stColumn = new JButton("2 to 1");
-		b1stColumn.setPreferredSize(new Dimension(80, 40));
-		b1stColumn.setForeground(Color.white);
-		b1stColumn.setBackground(FOREST_GREEN);
-		b1stColumn.setOpaque(true);
-		b1stColumn.setBorderPainted(true);
-		b1stColumn.setBorder(WHITE_BORDER);
-		b1stColumn.setFont(new Font("Arial", Font.PLAIN, 25));
-		
-		b2ndColumn = new JButton("2 to 1");
-		b2ndColumn.setPreferredSize(new Dimension(80, 40));
-		b2ndColumn.setForeground(Color.white);
-		b2ndColumn.setBackground(FOREST_GREEN);
-		b2ndColumn.setOpaque(true);
-		b2ndColumn.setBorderPainted(true);
-		b2ndColumn.setBorder(WHITE_BORDER);
-		b2ndColumn.setFont(new Font("Arial", Font.PLAIN, 25));
-		
-		b3rdColumn = new JButton("2 to 1");
-		b3rdColumn.setPreferredSize(new Dimension(80, 40));
-		b3rdColumn.setForeground(Color.white);
-		b3rdColumn.setBackground(FOREST_GREEN);
-		b3rdColumn.setOpaque(true);
-		b3rdColumn.setBorderPainted(true);
-		b3rdColumn.setBorder(WHITE_BORDER);
-		b3rdColumn.setFont(new Font("Arial", Font.PLAIN, 25));
+		for (int i = 37; i <= 39; i++) {
+			buttons[i].setPreferredSize(new Dimension(80, 40));
+			buttons[i].setBackground(FOREST_GREEN);
+		}
 
 		// add 1st-3rd dozen buttons
 		subBottom.setLayout(new GridLayout(2, 3));
 		subBottom.setPreferredSize(new Dimension(1200, 80));
-		b1stDozen = new JButton("1st 12");
-		b1stDozen.setForeground(Color.white);
-		b1stDozen.setBackground(FOREST_GREEN);
-		b1stDozen.setOpaque(true);
-		b1stDozen.setBorderPainted(true);
-		b1stDozen.setBorder(WHITE_BORDER);
-		b1stDozen.setFont(new Font("Arial", Font.PLAIN, 25));
-		
-		b2ndDozen = new JButton("2nd 12");
-		b2ndDozen.setForeground(Color.white);
-		b2ndDozen.setBackground(FOREST_GREEN);
-		b2ndDozen.setOpaque(true);
-		b2ndDozen.setBorderPainted(true);
-		b2ndDozen.setBorder(WHITE_BORDER);
-		b2ndDozen.setFont(new Font("Arial", Font.PLAIN, 25));
-		
-		b3rdDozen = new JButton("3rd 12");
-		b3rdDozen.setForeground(Color.white);
-		b3rdDozen.setBackground(FOREST_GREEN);
-		b3rdDozen.setOpaque(true);
-		b3rdDozen.setBorderPainted(true);
-		b3rdDozen.setBorder(WHITE_BORDER);
-		b3rdDozen.setFont(new Font("Arial", Font.PLAIN, 25));
-		
+		// for(int i = 40; i <=42;i++){
+		// buttons[i].setBackground(FOREST_GREEN);
+		// }
+
 		// add sub-bottom buttons
 		// 1-18/19-36, RED/BLACK, ODD/EVEN
 		subBottom1 = new JPanel();
@@ -245,56 +210,17 @@ public class RouletteView extends JFrame {
 		subBottom2.setLayout(new GridLayout(1, 2));
 		subBottom3.setLayout(new GridLayout(1, 2));
 
-		bOdd = new JButton("ODD");
-		// bOdd.setPreferredSize(new Dimension(40, 20));
-		bOdd.setForeground(Color.white);
-		bOdd.setBackground(FOREST_GREEN);
-		bOdd.setOpaque(true);
-		bOdd.setBorderPainted(true);
-		bOdd.setBorder(WHITE_BORDER);
-		bOdd.setFont(new Font("Arial", Font.PLAIN, 25));
+		for (int i = 40; i <= 48; i++) {
+			if (i != 45 && i != 46) {
+				buttons[i].setBackground(FOREST_GREEN);
+			}
 
-		bEven = new JButton("EVEN");
-		bEven.setForeground(Color.white);
-		bEven.setBackground(FOREST_GREEN);
-		bEven.setOpaque(true);
-		bEven.setBorderPainted(true);
-		bEven.setBorder(WHITE_BORDER);
-		bEven.setFont(new Font("Arial", Font.PLAIN, 25));
-		
-		b1stHalf = new JButton("1 to 18");
-		b1stHalf.setForeground(Color.white);
-		b1stHalf.setBackground(FOREST_GREEN);
-		b1stHalf.setOpaque(true);
-		b1stHalf.setBorderPainted(true);
-		b1stHalf.setBorder(WHITE_BORDER);
-		b1stHalf.setFont(new Font("Arial", Font.PLAIN, 25));
-		
-		b2ndHalf = new JButton("19 to 36");
-		b2ndHalf.setForeground(Color.white);
-		b2ndHalf.setBackground(FOREST_GREEN);
-		b2ndHalf.setOpaque(true);
-		b2ndHalf.setBorderPainted(true);
-		b2ndHalf.setBorder(WHITE_BORDER);
-		b2ndHalf.setFont(new Font("Arial", Font.PLAIN, 25));
-		
-		bRed = new JButton("RED");
-		bBlack = new JButton("BLACK");
-		
-		bRed.setForeground(Color.white);
-		bRed.setBackground(Color.red);
-		bRed.setOpaque(true);
-		bRed.setBorderPainted(true);
-		bRed.setBorder(WHITE_BORDER);
-		bRed.setFont(new Font("Arial", Font.PLAIN, 25));
-		
-		bBlack.setForeground(Color.white);
-		bBlack.setBackground(Color.black);
-		bBlack.setOpaque(true);
-		bBlack.setBorderPainted(true);
-		bBlack.setBorder(WHITE_BORDER);
-		bBlack.setFont(new Font("Arial", Font.PLAIN, 25));
-		
+		}
+
+		buttons[45].setBackground(Color.red);
+
+		buttons[46].setBackground(Color.black);
+
 		addButtonToPanel();
 	}
 
@@ -343,62 +269,101 @@ public class RouletteView extends JFrame {
 		subCenter.add(buttons[31]);
 		subCenter.add(buttons[34]);
 
-		subRight.add(b3rdColumn);
-		subRight.add(b2ndColumn);
-		subRight.add(b1stColumn);
+		for (int i = 39; i >= 37; i--) {
+			subRight.add(buttons[i]);
+		}
 
-		subBottom.add(b1stDozen);
-		subBottom.add(b2ndDozen);
-		subBottom.add(b3rdDozen);
+		for (int i = 40; i <= 42; i++) {
+			subBottom.add(buttons[i]);
+		}
 
 		subBottom.add(subBottom1);
 		subBottom.add(subBottom2);
 		subBottom.add(subBottom3);
 
-		subBottom1.add(b1stHalf);
-		subBottom1.add(bEven);
-		subBottom2.add(bRed);
-		subBottom2.add(bBlack);
-		subBottom3.add(bOdd);
-		subBottom3.add(b2ndHalf);
+		subBottom1.add(buttons[43]);
+		subBottom1.add(buttons[44]);
+		subBottom2.add(buttons[45]);
+		subBottom2.add(buttons[46]);
+		subBottom3.add(buttons[47]);
+		subBottom3.add(buttons[48]);
+	}
+
+	/**
+	 * Transfer button index to string
+	 * 
+	 * @param index
+	 * @return
+	 */
+	private String buttonIndexToString(int index) {
+		switch (index) {
+		case 37:
+			return "2 to 1";// 1stRow(bottom 1st)
+		case 38:
+			return "2 to 1";// 2ndRow
+		case 39:
+			return "2 to 1";// 3rdRow
+		case 40:
+			return "1st 12";
+		case 41:
+			return "2nd 12";
+		case 42:
+			return "3rd 12";
+		case 43:
+			return "1 - 18";
+		case 44:
+			return "EVEN";
+		case 45:
+			return "RED";
+		case 46:
+			return "BLACK";
+		case 47:
+			return "ODD";
+		case 48:
+			return "19 - 36";
+		default:
+			return "" + index;
+		}
 	}
 
 	/**
 	 * attach action listeners to buttons
 	 */
 	private void attachListenersToComponents() {
-		
+
 		for (int i = 0; i < buttons.length; i++) {
 			buttons[i].addActionListener(new ButtonEvent(i));
 		}
-		
+
 	}
-	
+
 	/**
 	 * inner class for single number betting
 	 */
 	private class ButtonEvent implements ActionListener {
-		
+
 		// instance variables
 		private int i;
 		private int clicked;
-		
+
 		/**
 		 * constructor
-		 * @param i button number
+		 * 
+		 * @param i
+		 *            button number
 		 */
 		public ButtonEvent(int i) {
 			this.i = i;
 			clicked = 0;
 		}
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			String text = "" + i + "\n" + "[$" + clicked + "]";
 			clicked++;
+			String text = "" + i + "\n" + "[$" + clicked + "]";
 			buttons[i].setText("<html>" + text.replaceAll("\\n", "<br>") + "</html>");
 		}
-		
+
 	}
 
 	/**
