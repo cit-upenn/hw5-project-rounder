223cdf2690589faf25a1ca9b57c6f97b6f3d1f7e
Zhiyuan Li <zhil@seas.upenn.edu>
2015-12-08 21:57:48 -0500

    lobby view refined

--- src/lobby/Lobby.java
+++ src/lobby/Lobby.java
@@ -1,7 +1,9 @@
 package lobby;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -13,13 +15,16 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 import keno.*;
 import roulette.*;
 import slot_machine.*;
 
 /**
- * This class represents the game lobby, which has 3 options: Roulette, Keno, Slot Machine.
+ * This class represents the game lobby, which has 3 options: Roulette, Keno,
+ * Slot Machine.
  * 
  * @author SHANG
  *
@@ -27,16 +32,20 @@ import slot_machine.*;
 public class Lobby extends JFrame {
 
 	private static final long serialVersionUID = 5008340198532190080L;
-	
+
 	// instance variables
-	
+
 	// GUI variables
 	private JPanel top, left, right, bottom, center;
 	private JButton roulette, keno, slot;
 	private JLabel lobbyView;
-	
+
+	private static final Border WHITE_BORDER = new LineBorder(Color.WHITE, 2);
+	private static final Color DARK_GREEN = new Color(0, 100, 0);
+	private static final Color MEDIUM_ORCHID = new Color(186, 85, 211);
+
 	/**
-	 * constructor 
+	 * constructor
 	 */
 	public Lobby() {
 		display();
@@ -49,7 +58,7 @@ public class Lobby extends JFrame {
 		setTitle("Lobby");
 		layOutComponents();
 		attachListenersToComponents();
-		setPreferredSize(new Dimension(1000, 400));
+		setPreferredSize(new Dimension(600, 400));
 		setLocation();
 		pack();
 		setVisible(true);
@@ -65,7 +74,7 @@ public class Lobby extends JFrame {
 		int y = (int) ((dimension.getHeight() / 2 - this.getHeight()) / 2);
 		setLocation(x, y);
 	}
-	
+
 	/**
 	 * helper method to add image to the view
 	 */
@@ -74,7 +83,7 @@ public class Lobby extends JFrame {
 		// adjust image size and add to view
 		ImageIcon image = new ImageIcon(getClass().getResource("lobby.jpg"));
 		Image img = image.getImage();
-		Image newImg = img.getScaledInstance(1000, 360, java.awt.Image.SCALE_SMOOTH);
+		Image newImg = img.getScaledInstance(600, 360, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon newImage = new ImageIcon(newImg);
 		lobbyView = new JLabel(newImage);
 		center.add(lobbyView);
@@ -107,29 +116,49 @@ public class Lobby extends JFrame {
 		add(bottom, BorderLayout.SOUTH);
 
 	}
-	
+
 	/**
 	 * helper method to add sub-panels within the bottom panel
 	 */
 	private void addSubPanels() {
 
 		bottom.setLayout(new GridLayout(1, 3));
+		bottom.setBackground(DARK_GREEN);
 
 		// add 3 option buttons
 		roulette = new JButton("Roulette");
+		setButton(roulette);
 		bottom.add(roulette);
 		keno = new JButton("Keno");
+		keno.setForeground(Color.white);
+		setButton(keno);
 		bottom.add(keno);
 		slot = new JButton("Slot Machine");
+		setButton(slot);
 		bottom.add(slot);
 
+	}
+	
+	
+	/**
+	 * set button attributes
+	 * @param button
+	 */
+	private void setButton(JButton button){
+		button.setForeground(Color.white);
+		button.setBackground(MEDIUM_ORCHID);
+		button.setOpaque(true);
+		button.setBorderPainted(true);
+		button.setBorder(WHITE_BORDER);
+		button.setFont(new Font("Arial", Font.PLAIN, 25));
+		
 	}
 
 	/**
 	 * attach action listeners to buttons
 	 */
 	private void attachListenersToComponents() {
-		
+
 		// add listeners to buttons
 		roulette.addActionListener(new ActionListener() {
 
@@ -138,9 +167,9 @@ public class Lobby extends JFrame {
 				setVisible(false);
 				new RouletteView().setVisible(true);
 			}
-			
+
 		});
-		
+
 		keno.addActionListener(new ActionListener() {
 
 			@Override
@@ -148,9 +177,9 @@ public class Lobby extends JFrame {
 				setVisible(false);
 				new KenoView().setVisible(true);
 			}
-			
+
 		});
-		
+
 		slot.addActionListener(new ActionListener() {
 
 			@Override
@@ -158,16 +187,16 @@ public class Lobby extends JFrame {
 				setVisible(false);
 				new SlotView().setVisible(true);
 			}
-			
+
 		});
 
 	}
-	
+
 	/**
 	 * test the class
 	 */
 	public static void main(String[] args) {
 		Lobby lobby = new Lobby();
 	}
-	
+
 }
